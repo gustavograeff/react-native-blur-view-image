b5b00429df70c29ef8f156222f8ab663e1a3cfc4
Gustavo Graeff <gustavograeff1998@gmail.com>
2023-07-22 19:16:43 -0300

    feat: create react native project

---
+++ src/components/Test.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const Test = () => {
+  return (
+    <View>
+      <Text>Hello World</Text>
+    </View>
+  );
+};
+
+export default Test;
