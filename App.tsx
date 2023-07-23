0c7dc0f6146f0da920455b223020591de39912c8
Gustavo Graeff <gustavograeff1998@gmail.com>
2023-07-22 23:23:36 -0300

    feat: add concept behind the compnent

--- App.tsx
+++ App.tsx
@@ -11,14 +11,12 @@ import {
 
 import plusIcon from './src/assets/icons/plus.png';
 import landscapeImage from './src/assets/landscape.jpg';
-import portraitImage from './src/assets/portrait.jpg';
-import squareImage from './src/assets/square.jpg';
 import { ImageBlur, ImageBlurView } from './src/components/ImageBlur';
 
 const App = (): JSX.Element => {
   return (
     <SafeAreaView>
-      <ScrollView contentInsetAdjustmentBehavior="automatic">
+      <ScrollView>
         <Text style={styles.description}>
           Only ImageBlurView will be blurred
         </Text>
@@ -44,210 +42,21 @@ const App = (): JSX.Element => {
             },
           ]}
         />
-
-        <Text style={styles.description}>Full width blur</Text>
-        <ImageBlur
-          src={portraitImage}
-          aspectRatio="portrait"
-          blurShapes={[
-            {
-              children: (
-                <ImageBlurView style={{ padding: 16 }}>
-                  <Text style={styles.imageText}>Some content</Text>
-                </ImageBlurView>
-              ),
-              position: { left: 0, right: 0 },
-            },
-          ]}
-        />
-
-        <Text style={styles.description}>Full height blur</Text>
-        <ImageBlur
-          src={squareImage}
-          aspectRatio="square"
-          blurShapes={[
-            {
-              children: (
-                <ImageBlurView style={{ height: '100%', padding: 16 }}>
-                  <Text style={styles.imageText}>Some content</Text>
-                </ImageBlurView>
-              ),
-              position: { top: 0, bottom: 0 },
-            },
-          ]}
-        />
-
-        <Text style={styles.description}>Full width / height blur</Text>
-        <ImageBlur
-          src={squareImage}
-          aspectRatio="square"
-          blurShapes={[
-            {
-              children: (
-                <ImageBlurView style={{ height: '100%', padding: 16 }}>
-                  <Text style={styles.imageText}>Some content</Text>
-                </ImageBlurView>
-              ),
-              position: { top: 0, bottom: 0, left: 0, right: 0 },
-            },
-          ]}
-        />
-
-        <Text style={styles.description}>Multiple ImageBlurView</Text>
-        <ImageBlur
-          src={landscapeImage}
-          aspectRatio="landscape"
-          blurShapes={[
-            {
-              children: (
-                <View
-                  style={{
-                    flexDirection: 'row',
-                    justifyContent: 'space-between',
-                    alignItems: 'flex-end',
-                    padding: 8,
-                  }}
-                >
-                  <View>
-                    <View>
-                      <Text style={styles.imageText}>Text 1</Text>
-                      <Text style={styles.imageText}>Text 2</Text>
-                    </View>
-                    <View
-                      style={{
-                        flexDirection: 'row',
-                        columnGap: 4,
-                        marginTop: 8,
-                      }}
-                    >
-                      <ImageBlurView
-                        style={{ padding: 16, alignSelf: 'flex-start' }}
-                        containerProps={{
-                          style: { borderRadius: 9999 },
-                        }}
-                      >
-                        <TouchableOpacity>
-                          <View
-                            style={{
-                              flexDirection: 'row',
-                              alignItems: 'center',
-                              columnGap: 4,
-                              alignSelf: 'flex-start',
-                            }}
-                          >
-                            <Image source={plusIcon} style={styles.image} />
-                            <Text style={styles.imageText}>
-                              The greater label
-                            </Text>
-                          </View>
-                        </TouchableOpacity>
-                      </ImageBlurView>
-                      <ImageBlurView
-                        style={{ padding: 16, alignSelf: 'flex-start' }}
-                        containerProps={{
-                          style: { borderRadius: 9999 },
-                        }}
-                      >
-                        <TouchableOpacity>
-                          <View
-                            style={{
-                              alignSelf: 'flex-start',
-                            }}
-                          >
-                            <Text style={styles.imageText}>Small label</Text>
-                          </View>
-                        </TouchableOpacity>
-                      </ImageBlurView>
-                    </View>
-                  </View>
-                  <View>
-                    <Text style={styles.imageText}>Text 3</Text>
-                    <Text style={styles.imageText}>Text 4</Text>
-                  </View>
-                </View>
-              ),
-              position: { left: 20, right: 20, bottom: 20 },
-            },
-          ]}
-        />
-
-        <Text style={styles.description}>Multiple blur shapes</Text>
-        <ImageBlur
-          src={portraitImage}
-          aspectRatio="portrait"
-          blurShapes={[
-            {
-              children: (
-                <ImageBlurView
-                  containerProps={{
-                    style: {
-                      borderTopLeftRadius: 8,
-                      borderBottomRightRadius: 30,
-                    },
-                  }}
-                  style={{
-                    padding: 16,
-                    alignSelf: 'flex-start',
-                    justifyContent: 'center',
-                  }}
-                >
-                  <Text style={styles.imageText}>Shape 1</Text>
-                </ImageBlurView>
-              ),
-              position: { left: '10%', top: '20%' },
-            },
-            {
-              children: (
-                <ImageBlurView
-                  containerProps={{
-                    style: {
-                      borderTopLeftRadius: 8,
-                      borderBottomRightRadius: 30,
-                    },
-                  }}
-                  style={{
-                    padding: 16,
-                    alignSelf: 'flex-start',
-                    justifyContent: 'center',
-                  }}
-                >
-                  <Text style={styles.imageText}>Shape 2</Text>
-                </ImageBlurView>
-              ),
-              position: { right: '10%', bottom: '20%' },
-            },
-          ]}
-        />
-
-        <Text style={styles.description}>Custom blur props</Text>
-        <ImageBlur
-          src={portraitImage}
-          aspectRatio="portrait"
-          blurShapes={[
-            {
-              children: (
-                <ImageBlurView
-                  containerProps={{
-                    blurRadius: 3,
-                    overlay: { backgroundColor: 'blue', opacity: 0.3 },
-                    style: {
-                      borderTopLeftRadius: 8,
-                      borderBottomRightRadius: 30,
-                    },
-                  }}
-                  style={{
-                    padding: 16,
-                    alignSelf: 'flex-start',
-                    justifyContent: 'center',
-                  }}
-                >
-                  <Text style={styles.imageText}>Custom blur props</Text>
-                </ImageBlurView>
-              ),
-              position: { left: 20, top: '20%' },
-            },
-          ]}
-        />
+        <Text />
+        <Text />
+        <Text />
+        <Text />
+        <Text />
+        <Text />
+        <Text />
+        <Text />
+        <Text />
+        <Text />
+        <Text />
+        <Text />
+        <Text />
+        <Text />
+        <Text />
       </ScrollView>
     </SafeAreaView>
   );
--- src/components/ImageBlur/ImageBlurShape.tsx
+++ src/components/ImageBlur/ImageBlurShape.tsx
@@ -1,18 +1,13 @@
-import React, { useCallback, useState } from 'react';
+import React from 'react';
 import {
   Image,
   type ImageResizeMode,
-  type LayoutRectangle,
-  Platform,
   type StyleProp,
   View,
   type ViewStyle,
 } from 'react-native';
 
-import useImageBlur from '../../hooks/useImageBlur';
 import { getImageSource } from '../../utils/image';
-
-import styles from './ImageBlur.styles';
 
 type BlurShapePosition = {
   bottom?: number | `${number}%`;
@@ -52,122 +47,38 @@ export type ImageBlurShapeProps = {
   resizeMode: ImageResizeMode;
 };
 
-const minIOSBlur = 5;
-
 const ImageBlurShape = ({
   children,
   containerRef,
   image,
   position,
+  resizeMode,
 }: ImageBlurShapeProps): JSX.Element => {
-  const { getRefList, getContainerProps } = useImageBlur();
-
-  const [childrenRect, setChildrenRect] = useState<LayoutRectangle[] | null>(
-    null,
-  );
-
-  const blurredElements = getRefList();
-  const childrenMounted =
-    blurredElements &&
-    childrenRect &&
-    childrenRect.length === blurredElements.length &&
-    childrenRect[0]?.width !== undefined &&
-    childrenRect[0]?.height !== undefined &&
-    (childrenRect[0]?.x !== undefined || childrenRect[0]?.y !== undefined);
-  const showBlur = blurredElements === null || childrenMounted;
-
-  const getBlurShape = (rect: LayoutRectangle | undefined, index: number) => {
-    const containerProps = getContainerProps(index);
-    const blurRadius =
-      containerProps?.blurRadius === undefined ? 16 : containerProps.blurRadius;
-
-    return (
-      <View
-        aria-hidden={!showBlur}
-        key={index}
-        style={[
-          containerProps?.style,
-          styles.blurShapeContainer,
-          styles.sticked,
-          !showBlur && styles.hide,
-          rect && {
-            height: rect.height,
-            width: rect.width,
-            top: rect.y,
-            left: rect.x,
-          },
-        ]}
-      >
-        <Image
-          source={getImageSource(image.src)}
-          style={[
-            {
-              width: image.width,
-              height: image.height,
-            },
-            rect && { top: -rect.y, left: -rect.x },
-          ]}
-          resizeMode="contain"
-          blurRadius={
-            blurRadius === 0
-              ? 0
-              : Platform.OS === 'android'
-              ? blurRadius
-              : blurRadius + minIOSBlur
-          }
-        />
-        <View
-          style={[
-            styles.sticked,
-            styles.fitAvailableSpace,
-            {
-              opacity: containerProps?.overlay?.opacity ?? 0.2,
-              backgroundColor:
-                containerProps?.overlay?.backgroundColor ?? '#000000',
-            },
-            !showBlur && styles.hide,
-          ]}
-        />
-      </View>
-    );
-  };
-
-  const calculateShapePosition = useCallback(() => {
-    if (!blurredElements) return;
-
-    blurredElements.forEach((elementRef, index) => {
-      if (!containerRef?.current) return;
-      if (!elementRef.current) return;
-
-      elementRef.current.measureLayout(
-        containerRef?.current,
-        (x, y, width, height) => {
-          setChildrenRect(prev => {
-            if (prev) {
-              prev[index] = { x, y, width, height };
-
-              return [...prev];
-            }
-
-            return [{ x, y, width, height }];
-          });
-        },
-      );
-    });
-  }, [blurredElements, containerRef]);
-
   return (
     <>
-      {childrenRect?.map((rect, index) => {
-        return getBlurShape(rect, index);
-      })}
-
       <View
-        aria-hidden={!showBlur}
-        style={[styles.sticked, !showBlur && styles.hide, position]}
-        onLayout={calculateShapePosition}
+        style={{
+          backgroundColor: 'black',
+          width: 100,
+          height: 150,
+          left: 100,
+          top: 40,
+          overflow: 'hidden',
+          // opacity: 0.6,
+        }}
       >
-        {children}
+        <Image
+          blurRadius={0}
+          source={getImageSource(image.src)}
+          style={{
+            width: image.width,
+            height: image.height,
+            left: -100,
+            top: -40,
+            opacity: 0.7,
+          }}
+          resizeMode={resizeMode}
+        />
       </View>
     </>
   );
